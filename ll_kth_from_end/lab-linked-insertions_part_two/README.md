5e73152530a44fdf66a04c2ae96fc935d495c919
edpuzino <edpuzino@yahoo.com>
2018-10-23 20:32:17 -0700

    fix ll

---
+++ ll_kth_from_end/lab-linked-insertions_part_two/README.md
@@ -0,0 +1,8 @@
+# Linked List Insertions part two kth value from the end of a linked list
+Code challenge 07 of JS 401n7
+
+## Challenge
+Write a method for the Linked List class which takes a number, k, as a parameter. Return the node’s value that is k from the end of the linked list. You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
+
+## Solution
+![](../../assets/ll_kth_from_end.jpg)    append(value) code
---
+++ ll_kth_from_end/lab-linked-insertions_part_two/lib/ll.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const Node = require('./node.js');
+
+class LinkedList {
+
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {     // Big O value is O(n)
+
+    let node = new Node(value);
+
+    // This happens if the list is empty
+    if(! this.head) {
+      this.head = node;
+      return this;
+    }
+
+    // If we have stuff, we add to the end
+    let currentNode = this.head;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+    return this;
+
+  }
+
+  prepend(value) {    // Big O value is O(2)
+
+    let node = new Node(value);
+
+    // This happens if the list is empty
+    if(! this.head) {
+      this.head = node;
+      return this;      
+    }
+
+    // If we have stuff, we add to the beginning
+    node.next = this.head;
+    this.head = node;
+    return this;
+  }
+
+  kFromEnd(k) {      // Big O value is O(n)
+    
+    let node = new Node(k);
+    // this happens if the list is empty
+    if(! this.head) {
+      this.head = node;
+      return this;
+    }
+
+    // If we have stuff, we return the value of the list item k from the end
+    let ctr = 0;
+    let currentNode = this.head;
+    while(currentNode.next) {
+      currentNode = currentNode.next;
+      ctr++;
+    }
+    currentNode = this.head;
+    for(let i = 0; i < (crt-k); i++) {}
+
+    currentNode= currentNode.next;
+    return currentNode.value;    
+  }
+
+/*
+  reverse() {}
+
+  remove(offset) {}
+
+  serialize() {}
+
+  deserialize() {}
+*/
+}
+
+module.exports = LinkedList;
