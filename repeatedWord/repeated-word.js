020caba149d4d7d78e74a82c54f355d8e9722428
edpuzino <edpuzino@yahoo.com>
2018-12-12 21:23:14 -0800

    coded

---
+++ repeatedWord/repeated-word.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+let repeatedWord = (string) => {
+  string = string.toLowerCase();
+  let arr = string.split(' ');
+  console.log(arr);
+  if(!arr) {
+    return 'The string is empty';
+  }
+  for(let i = 0; i < arr.length-1; i++) {
+    for(let j= i+1; j < arr.length; j++) {
+      if(arr[i] === arr[j]) {
+        console.log(arr[i]);
+        return arr[i];
+      }
+    }
+  }
+  console.log('There were no matches found');
+  return 'There were no matches found';
+}
