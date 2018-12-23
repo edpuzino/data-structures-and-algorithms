150a331accf44868ad143b4d1b74e58ca9465f7c
edpuzino <edpuzino@yahoo.com>
2018-12-22 20:28:03 -0800

    add picture access

--- array_reverse/array_reverse.js
+++ array_reverse/array_reverse.js
@@ -1,7 +1,7 @@
 'use strict';
 
 
-// Takes an array, loops through oit bacwards and pushes the elements to another array. When done the new array it copied into the original array and then returned.
+// Takes an array, loops through it backwards and pushes the elements to another array. When done the new array it copied into the original array and then returned.
 let reverseArray = arr => {
   let revArr = [];
   for(let i = arr.length-1; i >= 0; i--) {
